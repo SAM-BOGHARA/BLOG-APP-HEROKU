37b3b3ee3fb452df479776c45d19c2878fa41843
SAM-BOGHARA <88540024+SAM-BOGHARA@users.noreply.github.com>
2022-07-10 16:40:32 +0530

    error of numpy commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
-from numpy import size
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
